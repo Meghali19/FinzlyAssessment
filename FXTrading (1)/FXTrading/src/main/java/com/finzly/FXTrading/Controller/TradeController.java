103e89cc6a8613bae2b921c0e9ac027532c660e3
Meghali Likhar <93251510+Meghali19@users.noreply.github.com>
2023-09-11 18:25:59 +0530

    Update TradeController.java

--- FXTrading (1)/FXTrading/src/main/java/com/finzly/FXTrading/Controller/TradeController.java
+++ FXTrading (1)/FXTrading/src/main/java/com/finzly/FXTrading/Controller/TradeController.java
@@ -4,35 +4,70 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.finzly.FXTrading.Model.Trade;
 import com.finzly.FXTrading.Service.TradeService;
 
 @RestController
 @RequestMapping("trade")
-public class TradeController 
-{
-    @Autowired
-    private TradeService tradeservice;
-    
-    
-    @PostMapping("bookTrade")
-    public ResponseEntity<Map<String,String>> bookTrade(@RequestBody Trade trade)
-    {
-    	return tradeservice.bookTrade(trade);
-    }
-    
-    @GetMapping("getTrade")
-    public ResponseEntity<Object> getTrade()
-    {
-    	return tradeservice.getTrades();
-    }
+public class TradeController {
+	/*
+	 * Autowire the TradeService to handle trade-related operations
+	 */
+	@Autowired
+	private TradeService tradeservice;
+
+	/*
+	 * POST endpoint to book a trade.
+	 * 
+	 * @param trade The trade details provided in the request body.
+	 * 
+	 * @return ResponseEntity containing a map with a message indicating the status
+	 * of the trade booking.
+	 */
+	@PostMapping("bookTrade")
+	public ResponseEntity<Map<String, String>> bookTrade(@RequestBody Trade trade) {
+
+		try {
+			// Attempt to book the trade
+			return tradeservice.bookTrade(trade);
+		} catch (Exception ex) {
+			/*
+			 * Handle unexpected exceptions Return a 500 Internal Server Error response with
+			 * a generic error message
+			 */
+			Map<String, String> errorResponse = Map.of("error", "An unexpected error occurred.");
+			return ResponseEntity.status(500).body(errorResponse);
+		}
+	}
+
+	/*
+	 * GET endpoint to retrieve a list of trades.
+	 * 
+	 * @return ResponseEntity containing the list of trades or an error message if
+	 * no trades are available.
+	 */
+	@GetMapping("getTrade")
+	public ResponseEntity<Object> getTrade() {
+
+		try {
+			// Attempt to retrieve the list of trades
+			return tradeservice.getTrades();
+		} catch (Exception ex) {
+
+			/*
+			 * Handle unexpected exceptions Return a 500 Internal Server Error response with
+			 * a generic error message
+			 */
+			Map<String, String> errorResponse = Map.of("error", "An unexpected error occurred.");
+			return ResponseEntity.status(500).body(errorResponse);
+		}
+	}
 
 }
 
